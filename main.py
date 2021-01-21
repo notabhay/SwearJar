771066a368114f28630f0963ade33e5452dad5b1
Abhay <mail@notabhay.xyz>
2021-01-22 02:58:52 +0900

    Format

--- main.py
+++ main.py
@@ -2,52 +2,53 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if message.content.lower().startswith('.hello'):
-    await message.reply('Hello! <@' + str(message.author.id) + '>')
-    await message.add_reaction(":monkaStab:")
+    if message.content.lower().startswith('.hello'):
+        await message.reply('Hello! <@' + str(message.author.id) + '>')
+        await message.add_reaction(":monkaStab:")
 
-  if message.content.lower().startswith('.add'):
-  
+    if message.content.lower().startswith('.add'):
+
+        with open('blacklist.txt', 'r') as f:
+            words = f.read()
+            blacklist = words.split()
+
+        msg = message.content.lower()
+        for word in blacklist:
+            if word in msg:
+                await message.reply(message.content[5:] + ' already exists in the blacklist <@' + str(message.author.id) + '>')
+                return
+
+        with open("blacklist.txt", "a") as writefile:
+            writefile.write(' ' + message.content[5:])
+
+        with open('blacklist.txt', 'r') as f:
+            words = f.read()
+            blacklist = words.split()
+
+        await message.reply('Added <@' + str(message.author.id) + '>')
+        return
+
     with open('blacklist.txt', 'r') as f:
-      words = f.read()
-      blacklist = words.split()
-    
+        words = f.read()
+        blacklist = words.split()
+
     msg = message.content.lower()
     for word in blacklist:
-      if word in msg:
-        await message.reply(message.content[5:] + ' already exists in the blacklist <@' + str(message.author.id) + '>')
-        return
-    
-    with open("blacklist.txt", "a") as writefile:
-      writefile.write(' ' + message.content[5:])
-    
-    with open('blacklist.txt', 'r') as f:
-      words = f.read()
-      blacklist = words.split()
-      
-    await message.reply('Added <@' + str(message.author.id) + '>')
+        if word in msg:
+            await message.reply('Don\'t swear <@' + str(message.author.id) + '>')
+            # await message.delete()
+            return
     return
-
-
-  with open('blacklist.txt', 'r') as f:
-    words = f.read()
-    blacklist = words.split()
-
-  msg = message.content.lower()
-  for word in blacklist:
-    if word in msg:
-      await message.reply('Don\'t swear <@' + str(message.author.id) + '>')
-      # await message.delete()
-      return
-  return
 
 client.run('N'+'zk4Mzk3NDg2NzUzNzc1NjI3.X_0bmg.GNZVoeavJqvuQw_bFK23aMKoc9M')
